868b39b8bdafca197d4c3c8bbded7048e6f90901
Matias Pan <matias.pan@tiendanube.com>
2023-10-30 09:33:32 -0300

    No javaopts

--- main.go
+++ main.go
@@ -44,7 +44,8 @@ func (m *GradleService) BuildRuntime(ctx context.Context) *Container {
 		WithExec([]string{"wget", "-O", "dd-java-agent.jar", "https://dtdg.co/latest-java-tracer"}).
 		WithWorkdir("/app").
 		WithFile("app.jar", jar).
-		WithEntrypoint([]string{"sh", "-c", "java $JAVA_OPTS -jar app.jar --server.port=80 --spring.profiles.active=default"})
+		// WithEntrypoint([]string{"sh", "-c", "java $JAVA_OPTS -jar app.jar --server.port=80 --spring.profiles.active=default"})
+		WithEntrypoint([]string{"java", "-jar", "app.jar", "--server.port=80", "--spring.profiles.active=default"})
 }
 
 func (m *GradleService) Publish(ctx context.Context, registry, tag string) (string, error) {
